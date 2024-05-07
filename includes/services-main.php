d7f8341ff1367686addf28c7f4c4f512f3dc8c29
egorsv1985 <egorvs2009@gmail.com>
2024-05-07 14:18:37 +0300

    fix bugs with services section

--- includes/services-main.php
+++ includes/services-main.php
@@ -6,66 +6,66 @@
 			<div class="carousel-inner">
 				<div class="carousel-item active">
 					<div class=" row">
-						<div class="p-2 col-md-7 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-7 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services1.png') no-repeat 50% 50% / 98% 100%; ">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase ">Лазерная косметология </div>
 						</div>
-						<div class="p-2 col-md-5 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services2.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
 
 
-						<div class="p-2 col-md-5 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services3.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
-						<div class="p-2 col-md-7 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services5.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-7 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services5.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services5.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
 					</div>
 				</div>
 				<div class="carousel-item">
 					<div class=" row">
-						<div class="p-2 col-md-7 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-7 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services1.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
-						<div class="p-2 col-md-5 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services2.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
 
 
-						<div class="p-2 col-md-5 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services3.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
-						<div class="p-2 col-md-7 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services5.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-7 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services5.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services5.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
 					</div>
 				</div>
 				<div class="carousel-item">
 					<div class=" row">
-						<div class="p-2 col-md-7 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-7 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services1.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
-						<div class="p-2 col-md-5 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services2.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
 
 
-						<div class="p-2 col-md-5 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services3.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
-						<div class="p-2 col-md-7 position-relative">
-							<img src="<?= SITE_TEMPLATE_PATH; ?>/images/services5.png" class="d-block w-100" alt="Лазерная косметология ">
+						<div class="mb-3 col-md-7 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services5.png') no-repeat 50% 50% / 98% 100%;">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services5.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Лазерная косметология </div>
 						</div>
 					</div>
