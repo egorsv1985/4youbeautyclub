73a4d065df32732284060c1a7aa20e723083defd
egorsv1985 <egorvs2009@gmail.com>
2024-05-07 16:51:52 +0300

    added section news

---
+++ includes/news-main.php
@@ -0,0 +1,38 @@
+<section class="py-5 news-main">
+	<div class="container">
+		<div class="row">
+			<div class="pt-5 col-12 col-md-4 fs-24">
+				<h2 class="mb-4 fs-58 fw-700">Популярные статьи</h2>
+				<p>Мы используем передовые технологии и профессиональное оборудование для максимально эффективных и безопасных процедур.</p>
+				<div class="mt-5 btns">
+					<a href="#" class="px-4 py-3 btn btn-outline-dark top-btn btn-arrow fs-24">Все статьи</a>
+				</div>
+			</div>
+			<div class="pt-5 col-4 col-md-8">
+				<div class="p-5 pe-0 news-right position-relative fs-24 fs-600">
+					<div class="row">
+						<div class="col-12 position-relative">
+							<div class="cover"></div>
+							<a href="#" class="px-2 py-3 mb-5 news-item d-block ms-auto max-content text-uppercase me-5">
+								<div class="mb-5 news-num">1</div>
+								<p>Идеальный образ:<br> как подобрать<br> уход за кожей </p>
+							</a>
+						</div>
+						<div class="col-4">
+							<a href="#" class="px-2 py-3 mb-5 news-item d-inline-block text-uppercase ">
+								<div class="mb-5 news-num">2</div>
+								<p>Секреты<br> здоровья кожи:<br> важность ухода </p>
+							</a>
+						</div>
+						<div class="col-8">
+							<a href="#" class="px-2 py-3 mb-5 news-item d-inline-block text-uppercase ">
+								<div class="mb-5 news-num">3</div>
+								<p>Топ-5 тенденций в<br> мире косметологии:<br> новое в 2024 году</p>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
