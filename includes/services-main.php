ab130d440baaaba4af5f34c506fd42f296f1a249
egorsv1985 <egorvs2009@gmail.com>
2024-05-16 14:40:10 +0300

    added adaptive for services-main

--- includes/services-main.php
+++ includes/services-main.php
@@ -1,8 +1,7 @@
 <section class="py-5 services-main">
 	<div class="container">
 		<h2 class="mb-5 text-center fs-58 fw-700">Наши услуги</h2>
-
-		<div id="servicesCarousel" class="carousel slide services-slider">
+		<div id="servicesCarouselDesktop" class="carousel d-none d-lg-block slide services-slider">
 			<div class="carousel-inner">
 				<div class="carousel-item active">
 					<div class=" row">
@@ -14,8 +13,6 @@
 							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services2.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Эстетическая косметология </div>
 						</a>
-
-
 						<a href="#" class="mb-3 services-link col-md-5 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services3.png') no-repeat 50% 50% / 98% 100%;">
 							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services3.png" class="d-block w-98" alt="Лазерная косметология "> -->
 							<div class="label fs-32 fs-500 text-uppercase">Инъекционная косметология </div>
@@ -71,11 +68,48 @@
 					</div>
 				</div>
 			</div>
-			<button class="carousel-control-prev" type="button" data-bs-target="#servicesCarousel" data-bs-slide="prev">
+			<button class="carousel-control-prev" type="button" data-bs-target="#servicesCarouselDesktop" data-bs-slide="prev">
 				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 				<span class="visually-hidden">Previous</span>
 			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#servicesCarousel" data-bs-slide="next">
+			<button class="carousel-control-next" type="button" data-bs-target="#servicesCarouselDesktop" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+		<div id="servicesCarouselMobile" class="carousel d-block d-lg-none slide services-slider">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<div class=" row">
+						<a href="#" class=" services-link col-12 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services1.png') no-repeat 50% 50% / 98% 100%; ">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-98" alt="Лазерная косметология "> -->
+							<div class="label fs-32 fs-500 text-uppercase ">Лазерная косметология </div>
+						</a>						
+					</div>
+				</div>
+				<div class="carousel-item active">
+					<div class=" row">
+						<a href="#" class=" services-link col-12 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services2.png') no-repeat 50% 50% / 98% 100%; ">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-98" alt="Лазерная косметология "> -->
+							<div class="label fs-32 fs-500 text-uppercase ">Лазерная косметология </div>
+						</a>						
+					</div>
+				</div>
+				<div class="carousel-item active">
+					<div class=" row">
+						<a href="#" class=" services-link col-12 position-relative" style="height: 350px; background: url('<?= SITE_TEMPLATE_PATH; ?>/images/services3.png') no-repeat 50% 50% / 98% 100%; ">
+							<!-- <img src="<?= SITE_TEMPLATE_PATH; ?>/images/services1.png" class="d-block w-98" alt="Лазерная косметология "> -->
+							<div class="label fs-32 fs-500 text-uppercase ">Лазерная косметология </div>
+						</a>						
+					</div>
+				</div>
+				
+			</div>
+			<button class="carousel-control-prev " type="button" data-bs-target="#servicesCarouselMobile" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next " type="button" data-bs-target="#servicesCarouselMobile" data-bs-slide="next">
 				<span class="carousel-control-next-icon" aria-hidden="true"></span>
 				<span class="visually-hidden">Next</span>
 			</button>
