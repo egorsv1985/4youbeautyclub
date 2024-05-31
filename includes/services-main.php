360c1acd516fd5718c3ab57acda35c357d5efb5e
egorsv1985 <egorvs2009@gmail.com>
2024-05-31 16:05:48 +0300

    fix

--- includes/services-main.php
+++ includes/services-main.php
@@ -115,5 +115,60 @@
 			</button>
 		</div>
 	</div>
-
 </section>
+
+<!-- <?php
+
+
+$iblockId = 1; // Замените на ID вашего инфоблока
+
+// Получаем разделы инфоблока
+$arFilter = array('IBLOCK_ID' => $iblockId, 'GLOBAL_ACTIVE' => 'Y');
+$arSelect = array('ID', 'NAME');
+$rsSections = CIBlockSection::GetList(array('SORT' => 'ASC'), $arFilter, false, $arSelect);
+
+echo '<div id="servicesCarouselDesktop" class="carousel d-none d-lg-block slide services-slider">';
+echo '<div class="carousel-inner">';
+
+$isActive = true;
+
+while ($arSection = $rsSections->Fetch()) {
+    echo '<div class="carousel-item' . ($isActive ? ' active' : '') . '">';
+    $isActive = false;
+    echo '<div class="row">';
+
+    // Получаем элементы инфоблока в текущем разделе
+    $arFilterElements = array('IBLOCK_ID' => $iblockId, 'SECTION_ID' => $arSection['ID'], 'ACTIVE' => 'Y');
+    $arSelectElements = array('ID', 'NAME', 'DETAIL_PAGE_URL', 'PREVIEW_PICTURE');
+    $rsElements = CIBlockElement::GetList(array('SORT' => 'ASC'), $arFilterElements, false, false, $arSelectElements);
+
+    while ($arElement = $rsElements->Fetch()) {
+        $elementName = $arElement['NAME'];
+        $elementLink = $arElement['DETAIL_PAGE_URL'];
+        $elementPictureId = $arElement['PREVIEW_PICTURE'];
+
+        // Получаем URL картинки
+        $arFile = CFile::GetFileArray($elementPictureId);
+        $elementPictureUrl = $arFile['SRC'];
+
+        echo '<a href="' . $elementLink . '" class="mb-3 services-link col-md-5 position-relative" style="height: 350px; background: url(\'' . $elementPictureUrl . '\') no-repeat 50% 50% / 98% 100%;">';
+        echo '<div class="label fs-32 fs-500 text-uppercase">' . $elementName . '</div>';
+        echo '</a>';
+    }
+
+    echo '</div>'; // .row
+    echo '</div>'; // .carousel-item
+}
+
+echo '</div>'; // .carousel-inner
+
+echo '<button class="carousel-control-prev" type="button" data-bs-target="#servicesCarouselDesktop" data-bs-slide="prev">';
+echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+echo '<span class="visually-hidden">Previous</span>';
+echo '</button>';
+echo '<button class="carousel-control-next" type="button" data-bs-target="#servicesCarouselDesktop" data-bs-slide="next">';
+echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
+echo '<span class="visually-hidden">Next</span>';
+echo '</button>';
+echo '</div>'; // #servicesCarouselDesktop
+?> -->
